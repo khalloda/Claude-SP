dc9a02f8fc40856a28ebd92cfa69c208ad3b3f15
khalloda <44370311+khalloda@users.noreply.github.com>
2025-08-21 14:55:36 +0300

    Update Client.php

--- app/models/Client.php
+++ app/models/Client.php
@@ -13,9 +13,9 @@ class Client extends Model
     protected array $fillable = [
         'type',
         'name',
-        'phone',
+        'phone',      // Added this
         'email',
-        'address'
+        'address'     // Added this
     ];
 
     public function search(string $query, int $page = 1, int $perPage = 15): array
