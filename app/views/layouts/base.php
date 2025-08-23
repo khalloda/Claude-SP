18f0313d5b2009962fe3af688a688bb13d2e9cc6
khalloda <44370311+khalloda@users.noreply.github.com>
2025-08-24 00:23:47 +0300

    Update base.php

--- app/views/layouts/base.php
+++ app/views/layouts/base.php
@@ -7,9 +7,9 @@ use App\Core\Helpers;
 <head>
     <meta charset="UTF-8">
 	<!-- FontAwesome Icons -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?? I18n::t('app.name') ?></title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="<?= Helpers::asset('css/app.css') ?>">
 	    <!-- Currency Configuration for JavaScript -->
     <script>
@@ -103,3 +103,4 @@ use App\Core\Helpers;
     </script>
 </body>
 </html>
+
